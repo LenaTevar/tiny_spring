d4b0753f6dc9983b478977abe2a1460f88dbc653
Helena Tevar <13490448+LenaTevar@users.noreply.github.com>
2021-07-18 09:51:10 +0200

    First Commit

---
+++ tinyboilerplate/src/main/java/com/tinyboilerplate/tinyboilerplate/TinyboilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.tinyboilerplate.tinyboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyboilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyboilerplateApplication.class, args);
+	}
+
+}
---
+++ tinyboilerplate/src/test/java/com/tinyboilerplate/tinyboilerplate/TinyboilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyboilerplate.tinyboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyboilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
